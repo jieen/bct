f8717ec01691b4058ac7a366bf9d6889d8d985d9
wujindev <wujindev@gmail.com>
2013-12-15 13:24:35 +0800

    bct init
    baidu cloud terminal init first

---
+++ src/com/jieen/bct/BctMain.java
@@ -0,0 +1,36 @@
+package com.jieen.bct;
+
+import com.jieen.bct.Pcs.pscMain;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class BctMain extends Activity {
+
+	private Button btnPcsMain = null;
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.bctmain);
+		
+		btnPcsMain = (Button) findViewById(R.id.btnPcs);
+		btnPcsMain.setOnClickListener(btnMainClickListener);
+		
+	}
+	//主页面按钮统一处理函数
+	private OnClickListener btnMainClickListener = new OnClickListener() {
+		public void onClick(View v) {
+			switch(v.getId())
+			{
+			case R.id.btnPcs:	//进入PCS主页按钮
+				Intent pcsIntent = new Intent(BctMain.this,pscMain.class);
+				startActivity(pcsIntent);
+				break;
+			}
+		}
+	};
+	
+}
---
+++ src/com/jieen/bct/Pcs/pscMain.java
@@ -0,0 +1,14 @@
+package com.jieen.bct.Pcs;
+
+import com.jieen.bct.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class pscMain extends Activity {
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.pcsmain);
+	}
+}
